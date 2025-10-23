2ac51522eac073f236dc58bcc80e9cced54a94ae
Andre Rodrigues <andreptrodrigo@gmail.com>
2025-10-24 00:36:00 +0100

    feat: add da id

--- src/components/Analytics/index.jsx
+++ src/components/Analytics/index.jsx
@@ -1,13 +1,9 @@
-"use client";
-
-import { useEffect } from "react";
 import { GoogleAnalytics } from "@next/third-parties/google";
-import Hotjar from "@hotjar/browser";
 
 const Analytics = () => {
   return (
     <>
-      <GoogleAnalytics gaId="YOUR_GA_ID" />
+      <GoogleAnalytics gaId="G-RPRJWVDP43" />
     </>
   );
 };
