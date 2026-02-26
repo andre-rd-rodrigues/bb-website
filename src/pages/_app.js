0c61de3654f402cb8592566e230d5a77dc4265d6
Andre Rodrigues <andreptrodrigo@gmail.com>
2026-02-26 12:36:11 +0000

    feat: add ts

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -20,6 +20,7 @@ export default function App({ Component, pageProps }) {
       <NextIntlClientProvider
         locale={router.locale}
         messages={pageProps.messages}
+        timeZone="Europe/Lisbon"
       >
         <Layout>
           <Component {...pageProps} />
